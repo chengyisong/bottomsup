6437df820d30c6cf0da18aea04a2add16ba284e0
GitWilder <43479032+Wilder101@users.noreply.github.com>
2018-11-28 15:05:03 -0800

    Updated app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,7 +13,7 @@
 //   };
 //   firebase.initializeApp(config);
 
-var config = {
+var configAdd = {
     apiKey: "AIzaSyCFqAd4UK2fTREHACjWb725Pvp3pKI4PwM",
     authDomain: "uwbottomsup.firebaseapp.com",
     databaseURL: "https://uwbottomsup.firebaseio.com",
@@ -22,10 +22,10 @@ var config = {
     messagingSenderId: "844557366354"
   };
 
-firebase.initializeApp(config);
+firebase.initializeApp(configAdd);
 
 // Create a variable to reference the database
-var database = firebase.database();
+var databaseAdd = firebase.database();
 
 // Variable for user submitted drinks
 let userSubmittedDrink = {          
@@ -33,15 +33,11 @@ let userSubmittedDrink = {
     // Property-value pairs
     userName: "",                   // Name of user
     nameOfDrink: "",                // Name of beverage
-    ingredients: [{                 // Ingredients array
-        ingredientQuantity: 0,      // Quantity of ingredient
-        ingredientUnits: "",        // Units of ingredient
-        ingredientName: ""          // Name of ingredient
-    }],
+    ingredients: [],                // Ingredients array
     instructions: ""                // Instructions for recipe
 };
 
-// Capture Button Click for recipe submission
+// Capture button click for recipe submission
 $("#submit-drink-button").on("click", function(event) {
 
     // Don't refresh the page!
@@ -50,18 +46,52 @@ $("#submit-drink-button").on("click", function(event) {
     // Capture inputs locally to push to Firebase database
     userSubmittedDrink.userName = $("#user-name-input").val().trim();
     userSubmittedDrink.nameOfDrink = $("#drink-name-input").val().trim();
-    userSubmittedDrink
-    
+    userSubmittedDrink.instructions = $("#ingredient-input-instructions").val().trim();
+
+    // Loop through ingredients inputs
+    for (let i = 1; i <= 5; i++) {
+
+        // Local variables for data input gathering
+        let quantityID   = "#ingredient-input-quantity-" + i;
+        let unitsID      = "#ingredient-input-units-" + i;
+        let ingredientID = "#ingredient-input-name-" + i;
+
+        // Get user input values
+        // let tempQuantity = 0
+        // tempQuantity = $(quantityID).val().trim();
+
+        let tempQuantity = $(quantityID).val().trim();
+
+        if (tempQuantity != "") {
+        // if (typeof tempQuantity == 'undefined') {
+            
+            // build an ingredient array element and push it
+            let tempUnits = $(unitsID).val().trim();
+            let tempIngredientName = $(ingredientID).val().trim();
+
+            let ingredientPush = {
+                ingredientQuantity: tempQuantity,      
+                ingredientUnits: tempUnits,     
+                ingredientName: tempIngredientName   
+            };
+
+            userSubmittedDrink.ingredients.push(ingredientPush);
+            
+        } // End quantity if statement
+    } // End ingredient gathering loop
+
+    // testing
+    console.log(userSubmittedDrink);
 
     // Validate user input
+    // TO BE WRITTEN
     
 
     // Update Firebase database
-    database.ref().push({                   // Using .push from .set for data additions over replacement
-        name: name,
-        destination: destination,
-        firstTrainTime: firstTrainTime,
-        frequency: frequency
+    databaseAdd.ref().push({                   // Using .push from .set for data additions over replacement
+
+        userSubmittedDrink: userSubmittedDrink 
     });
-}); // End add train submit button click event handler
+
+}); // End Capture button click for recipe submission
 
